6f4f0b07b603563ea945728ed8d45eac73349d31
Karim <cseed322@gmail.com>
2020-11-27 19:33:22 +0300

    Updates

---
+++ precache-manifest.a1daab9ea95989598069ce62920b0e2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc1e9f69504c64d7ed70bd245147f5e",
+    "url": "/saga-test/index.html"
+  },
+  {
+    "revision": "5f60791f5c511dc66731",
+    "url": "/saga-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d065ba4463e9e4612c0f",
+    "url": "/saga-test/static/js/2.20b89a0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/saga-test/static/js/2.20b89a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f60791f5c511dc66731",
+    "url": "/saga-test/static/js/main.b83f9823.chunk.js"
+  },
+  {
+    "revision": "5e85a0b32444ea349ba6",
+    "url": "/saga-test/static/js/runtime-main.cc5accdb.js"
+  }
+]);
